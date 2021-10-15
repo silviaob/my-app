c26a3ae321ec63f44685a06e9dcea57623dafd91
Silvaob <silvia_ob14@hotmail.com>
2021-10-15 19:23:17 +0200

    mostrar listado pokemons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
+  const [pokemonList, setPokemonList] = useState();
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=1")
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemonList(data.results);
+      });
+  }, []);
+
+  const listItems = pokemonList.map((item) => <li>{item.name}</li>);
+
   return (
     <>
       <input type="text" id="fname" name="fname" value="John" readonly></input>
+      <ul>{listItems}</ul>
     </>
   );
 }
